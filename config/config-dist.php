cf5ec0bf1ad70d31f2650faeed8edc263cf338e7
yndj <46798345+yndj@users.noreply.github.com>
2019-10-31 05:55:58 +0800

    Update config-dist.php

--- config/config-dist.php
+++ config/config-dist.php
@@ -8,9 +8,9 @@ return array(
     /* 数据库设置 */
     'DB_TYPE'               =>  'mysqli',     // 数据库类型
     'DB_HOST'               =>  'localhost', // 服务器地址
-    'DB_NAME'               =>  'xcx',          // 数据库名
-    'DB_USER'               =>  'root',      // 用户名
-    'DB_PWD'                =>  'root',          // 密码
+    'DB_NAME'               =>  'juclnkth',          // 数据库名
+    'DB_USER'               =>  'juclnkth',      // 用户名
+    'DB_PWD'                =>  'uk52Tc20sM',          // 密码
     'DB_PORT'               =>  '3306',        // 端口
     'DB_PREFIX'             =>  'xcx_',    // 数据库表前缀
     'DB_PARAMS'          	=>  array(), // 数据库连接参数    
